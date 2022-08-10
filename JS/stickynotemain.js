a8bbcc76feec3e9476dc578a0a37cae453b8187b
PC <kim000183@naver.com>
2022-08-10 22:15:05 +0900

    드래그 시작 진행중

--- JS/stickynotemain.js
+++ JS/stickynotemain.js
@@ -1,11 +1,10 @@
 window.onload = function(){
 
-    const $body = document.querySelector('body');
     let cursorx;
     let cursory;
     let count = 1;
     // 마우스 위치 찾기
-    $body.addEventListener("mousemove", (event) => {
+    document.addEventListener("mousemove", (event) => {
         cursorx = `${event.pageY}px`;
         cursory = `${event.pageX}px`;
     });
@@ -22,37 +21,60 @@ window.onload = function(){
         event.preventDefault();
     });
 
-     // 메모추가 함수
-     function addnote(){
-        let newdiv = document.createElement("div");
+    // 메모추가 함수
+    function addnote(){
+        const newdiv = document.createElement("div");
         newdiv.classList.add(`note-box${count}`);
     
-        let clisediv = document.createElement("div");
-        let closebtn = document.createElement("button");
-        let closetxt = document.createTextNode('X'); 
+        const topbox = document.createElement("div");
+        const closebtn = document.createElement("button");
+        const closetxt = document.createTextNode('X'); 
 
         closebtn.appendChild(closetxt);
         closebtn.onclick = delnote;
 
-        clisediv.appendChild(closebtn);
-
-        let newtextarea = document.createElement("textarea");
+        const newtextarea = document.createElement("textarea");
         newtextarea.placeholder = '메모를 입력하세요...';
     
-        newdiv.appendChild(clisediv);
+        newdiv.appendChild(topbox);
+        newdiv.appendChild(closebtn);
         newdiv.appendChild(newtextarea);
-        $body.appendChild(newdiv);
+        document.body.appendChild(newdiv);
         
         newdiv.style.top = cursorx;
         newdiv.style.left = cursory;
-        
+
         count ++;
+
+        // 드래그앤 드랍
+        let isDragging;
+        let findX;
+        let findY;
+
+        topbox.addEventListener('mousedown', drageStart);
+        document.addEventListener('mouseup', drageEnd);
+        document.addEventListener('mousemove', drageMove);
+
+        // 드래그 시작
+        function drageStart(event){
+            if(event.button === 0){
+                findX = event.pageX - topbox.getBoundingClientRect().left;
+                findY = event.pageY - topbox.getBoundingClientRect().top;
+                isDragging = true
+                document.body.append(newdiv);
+            }
+        }
+
+      
+
     }
 
-      // 메모삭제 함수
-      function delnote(){
-        this.parentNode.parentNode.remove();
+    // 메모삭제 함수
+    function delnote(){
+        this.parentNode.remove();
     }
 
-    
+ 
+
+
 }
