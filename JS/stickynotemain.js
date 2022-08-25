23bbaeaf4917d2f56ed651a043f7451e17d50c3e
PC <kim000183@naver.com>
2022-08-25 23:39:08 +0900

    배열구조

--- JS/stickynotemain.js
+++ JS/stickynotemain.js
@@ -5,36 +5,49 @@ let findX;
 let findY;
 let savenote;
 
-function createMemo() {
-  const createDiv = document.createElement("div");
-  const headbox = document.createElement("div");
-  const closebtn = document.createElement("button");
-  const closetxt = document.createTextNode('X');
-  const newtextarea = document.createElement("textarea");
+const arr = []
 
-  newtextarea.placeholder = '메모를 입력하세요...';  
+// localStorage.setItem("key", arr);
+
+function render(){
+    const addnote = document.querySelector('body').innerHTML = arr.map((content) => `<div style="left:${content.left}px; top:${content.top}px"><div></div><button>X</button><textarea style="width:${content.width}px; height:${content.width}px;"></textarea></div>`);
+}
+
+function createMemo() {
+  // const createDiv = document.createElement("div");
+  // const headbox = document.createElement("div");
+  // const closebtn = document.createElement("button");
+  // const closetxt = document.createTextNode('X');
+  // const newtextarea = document.createElement("textarea");
+
+  // newtextarea.placeholder = '메모를 입력하세요...';  
   
-  closebtn.appendChild(closetxt);
-  createDiv.appendChild(headbox);
-  createDiv.appendChild(closebtn);
-  createDiv.appendChild(newtextarea);
-  document.body.appendChild(createDiv);
+  // closebtn.appendChild(closetxt);
+  // createDiv.appendChild(headbox);
+  // createDiv.appendChild(closebtn);
+  // createDiv.appendChild(newtextarea);
+  // document.body.appendChild(createDiv);
   
-  createDiv.style.top = cursory;
-  createDiv.style.left = cursorx;
+  // createDiv.style.top = cursory;
+  // createDiv.style.left = cursorx;
+  arr.push({width : 200, height : 116, left : cursorx, top : cursory})
+  render();
 }
 
 function onMousedown(event) {
   if (event.button == 2) {
     createMemo();
   } else if (event.target.tagName === 'BUTTON') {
+    console.log(arr)
     event.target.parentNode.remove();
+    arr.finIndex((i) => event.target)
   } else if (event.button === 0 && event.target.tagName === 'DIV') {
     findX = event.pageX - event.target.parentNode.getBoundingClientRect().left;
     findY = event.pageY - event.target.parentNode.getBoundingClientRect().top;
     isDragging = true
     document.body.append(event.target.parentNode);
   }
+
   savenote = document.body.innerHTML;
   localStorage.setItem("stickynote", savenote);
 }
@@ -49,12 +62,13 @@ function onMouseup(event){
 
 function onMousemove(event){
   const movetarget = document.querySelector('body > div:last-of-type');
-  cursorx = `${event.pageX}px`;
-  cursory = `${event.pageY}px`;
+  cursorx = `${event.pageX}`;
+  cursory = `${event.pageY}`;
   if (isDragging) {
     movetarget.style.top = `${event.pageY - findY - 8}px`;
     movetarget.style.left = `${event.pageX - findX - 8}px`;
   }
+
   savenote = document.body.innerHTML;
   localStorage.setItem("stickynote", savenote);
 }
