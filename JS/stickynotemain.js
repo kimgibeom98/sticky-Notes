6c00c5b53ec5001e857828fa9556f0528dd49ef2
PC <kim000183@naver.com>
2022-08-28 18:50:02 +0900

    수정

--- JS/stickynotemain.js
+++ JS/stickynotemain.js
@@ -3,26 +3,23 @@ let cursory;
 let isDragging;
 let findX;
 let findY;
-let savenote;
 
-const arr = []
+let data;
+let arr = []
 
-// localStorage.setItem("key", arr);
+// localStorage.setItem("key", data);
 
 function render(){
-    const addnote = document.querySelector('body').innerHTML = arr.map((content) => `<div style="left:${content.left}px; top:${content.top}px"><div></div><button>X</button><textarea style="width:${content.width}px; height:${content.width}px;"></textarea></div>`);
+    data =  document.querySelector('body').innerHTML = arr.map((content) => `<div class="note-box" style="left:${content.left}px; top:${content.top}px"><div></div><button>X</button><textarea style="width:${content.width}px; height:${content.height}px;"></textarea></div>`);
+    console.log(data)
 }
 
-
-
 function onMousedown(event) {
-  if (event.button == 2) {
+  if (event.button == 2){
     arr.push({width : 200, height : 116, left : cursorx, top : cursory})
     render();
-  } else if (event.target.tagName === 'BUTTON') {
-    console.log(arr)
+  }else if (event.target.tagName === 'BUTTON') {
     event.target.parentNode.remove();
-    arr.finIndex((i) => event.target)
   } else if (event.button === 0 && event.target.tagName === 'DIV') {
     findX = event.pageX - event.target.parentNode.getBoundingClientRect().left;
     findY = event.pageY - event.target.parentNode.getBoundingClientRect().top;
@@ -30,8 +27,7 @@ function onMousedown(event) {
     document.body.append(event.target.parentNode);
   }
 
-  savenote = document.body.innerHTML;
-  localStorage.setItem("stickynote", savenote);
+  localStorage.setItem("stickynote", data);
 }
 
 function onMouseup(event){
@@ -51,8 +47,22 @@ function onMousemove(event){
     movetarget.style.left = `${event.pageX - findX - 8}px`;
   }
 
-  savenote = document.body.innerHTML;
-  localStorage.setItem("stickynote", savenote);
+  localStorage.setItem("stickynote", data);
+}
+
+function onKeydown(event){
+  if(event.target.tagName === 'TEXTAREA'){
+    resizeEvent(event);
+    // arr.push(`<div style="left:${content.left}px; top:${content.top}px"><div></div><button>X</button><textarea style="width:${content.width}px; height:${content.height}px;">${event.tagName.value}</textarea></div>`)
+  }
+}
+
+function resizeEvent(event){
+  // arr = [];
+  for(let i = 0; data.length; i++){
+      arr.push(width.offsetWidth, height.data[i].offsetHeight, left.data[i].getBoundingClientRect().left, top.getBoundingClientRect().top, contents.event.target.value)
+  }
+  console.log(arr)
 }
 
 const savekey = localStorage.getItem('stickynote');
@@ -62,3 +72,4 @@ document.addEventListener('contextmenu', (event) => {event.preventDefault();});
 document.addEventListener('mousedown', onMousedown);
 document.addEventListener('mouseup', onMouseup);
 document.addEventListener('mousemove', onMousemove);
+document.addEventListener('keydown', onKeydown);
