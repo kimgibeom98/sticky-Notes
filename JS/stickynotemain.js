22599e45f79cbc09d4cb157d5b39604a7eb5e152
PC <kim000183@naver.com>
2022-08-31 22:14:00 +0900

    수정중

--- JS/stickynotemain.js
+++ JS/stickynotemain.js
@@ -4,16 +4,17 @@ let isDragging;
 let findX;
 let findY;
 let savenote;
-const data = localStorage.getItem('stickynote') ?? [];
+let data = [];
+// data = localStorage.getItem('stickynote') ?? [];
 
 
 function render(){
-  document.querySelector('body').innerHTML = arr.map((content) => `<div class="note-box" style="left:${content.left}px; top:${content.top}px"><div></div><button>X</button><textarea style="width:${content.width}px; height:${content.height}px;"></textarea></div>`);
+  document.querySelector('body').innerHTML = data.map((content) => `<div class="note-box" style="left:${content.left}px; top:${content.top}px"><div></div><button>X</button><textarea style="width:${content.width}px; height:${content.height}px;"></textarea></div>`);
 }
 
 function onMousedown(event) {
   if (event.button == 2) {
-    arr.push({width : 200, height : 116, left : cursorx, top : cursory})
+    data.push({width : 200, height : 116, left : cursorx, top : cursory})
     render();
   } else if (event.target.tagName === 'BUTTON') {
     event.target.parentNode.remove();
@@ -47,7 +48,7 @@ function onMousemove(event){
   localStorage.setItem("stickynote", savenote);
 }
 
-function onKeydown(event){
+function onKeyup(event){
   if(event.target.tagName === 'TEXTAREA'){
     data.push({width : event.target.offsetWidth, height : event.target.offsetHeight, left : findbox.getBoundingClientRect().left, top : findbox.getBoundingClientRect().top} );
   }
@@ -61,4 +62,4 @@ document.addEventListener('contextmenu', (event) => {event.preventDefault();});
 document.addEventListener('mousedown', onMousedown);
 document.addEventListener('mouseup', onMouseup);
 document.addEventListener('mousemove', onMousemove);
-document.addEventListener('keyup', onKeydown);
+document.addEventListener('keyup', onKeyup);
